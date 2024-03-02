1b95464272c17062ef9d56d708e715280c9f57eb
asimsharf <asimsahrf@gmail.com>
2024-03-02 15:47:28 +0300

    Initial Commit

---
+++ UberSwiftUI/UberSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUIApp.swift
+//  UberSwiftUI
+//
+//  Created by asimsharf on 02/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
